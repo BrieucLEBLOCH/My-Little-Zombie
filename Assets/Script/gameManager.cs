7740af166160d6ccd9527d7db0992e4d8a3be01c
unknown <icallewaert@gamingcampus.local>
2023-02-27 16:07:23 +0100

    add jump & clean HUD code

--- Assets/Script/gameManager.cs
+++ Assets/Script/gameManager.cs
@@ -8,6 +8,24 @@ using System;
 
 public class gameManager : MonoBehaviour
 {
+    [SerializeField] public float _health = 10;
+    [SerializeField] public float _healthMax = 10;
+    [SerializeField] public Component _healthComponent;
+
+    [SerializeField] public float _stamina = 100;
+    [SerializeField] public float _staminaMax = 100;
+    [SerializeField] public Component _staminaComponent;
+
+    [SerializeField] public float _food = 10;
+    [SerializeField] public float _foodMax = 10;
+    [SerializeField] public Component _foodComponent;
+
+    [SerializeField] public float _timeFoodDeleteAll = 10.0f;
+
+    public float _lastTimeStamina;
+    [SerializeField] public float _timeStaminaGain = 1.0f;
+    [SerializeField] public float _timeStaminaMaxUse = 5.0f;
+
     [SerializeField] public int _childSave = 0;
     private int _childSaveMax = 0;
     [SerializeField] Text _textChildSave;
@@ -32,17 +50,43 @@ public class gameManager : MonoBehaviour
             Cursor.lockState = CursorLockMode.Confined;
         }
 
-        UpdateChildUI();
+        UpdateUI();
 
         if (_childSave == _childSaveMax)
         {
             SceneManager.LoadScene("MenuWin");
         }
+
+        if (_health <= 0)
+        {
+            SceneManager.LoadScene("MenuOver");
+        }
     }
 
-    private void UpdateChildUI()
+    private void UpdateUI()
     {
         // Update Text Quest
         _textChildSave.text = "Enfants : " + _childSave.ToString() + " / " + _childSaveMax.ToString();
+        UpdateHealth();
+        UpdateStamina();
+        UpdateFood();
+    }
+
+    private void UpdateHealth()
+    {
+        float pourcentHealth = _health * 100 / _healthMax;
+        _healthComponent.transform.localScale = new Vector3(pourcentHealth * 2 / 100, 0.2f, 1f);
+    }
+
+    private void UpdateStamina()
+    {
+        float pourcentStamina = _stamina * 100 / _staminaMax;
+        _staminaComponent.transform.localScale = new Vector3(pourcentStamina * 2 / 100, 0.2f, 1f);
+    }
+
+    private void UpdateFood()
+    {
+        float pourcentFood = _food * 100 / _foodMax;
+        _foodComponent.transform.localScale = new Vector3(pourcentFood * 2 / 100, 0.2f, 1f);
     }
 }
--- Assets/Script/scriptPlayer.cs
+++ Assets/Script/scriptPlayer.cs
@@ -20,38 +20,15 @@ public class scriptPlayer : MonoBehaviour
     public bool _isGrounded;
     Rigidbody _rb;
 
-    [SerializeField] private float _health = 10;
-    [SerializeField] private float _healthMax = 10;
-    [SerializeField] Component _healthComponent;
-
-    [SerializeField] private float _stamina = 100;
-    [SerializeField] private float _staminaMax = 100;
-    [SerializeField] Component _staminaComponent;
-
-    [SerializeField] private float _food = 10;
-    [SerializeField] private float _foodMax = 10;
-    [SerializeField] Component _foodComponent;
-
-    [SerializeField] public int _childSave = 0;
-    private int _childSaveMax = 0;
-    [SerializeField] Text _textChildSave;
-
-    [SerializeField] private float _timeFoodDeleteAll = 10.0f;
-
-    private float _lastTimeStamina;
-    [SerializeField] private float _timeStaminaGain = 1.0f;
-    [SerializeField] private float _timeStaminaMaxUse = 5.0f;
+    //stats
+    GameObject gameManager;
+    gameManager gameManagerScript;
 
     // Animations
     Animator teacherAnim;
 
     [SerializeField] private bool bIsInDistributeur = false;
     [SerializeField] private bool bIsInHospital = false;
-    [SerializeField] private bool bIsInChildSave = false;
-
-    [SerializeField] private Collider childGameCollider;
-
-    GameObject[] listChild;
 
     // Start is called before the first frame update
     void Start()
@@ -61,8 +38,8 @@ public class scriptPlayer : MonoBehaviour
 
         _isGrounded = true;
 
-        listChild = GameObject.FindGameObjectsWithTag("Child");
-        _childSaveMax = listChild.Length;
+        gameManager = GameObject.Find("Manager");
+        gameManagerScript = gameManager.GetComponent<gameManager>();
     }
 
     void Update()
@@ -70,19 +47,19 @@ public class scriptPlayer : MonoBehaviour
         _speed = 5.0f;
 
         // Run
-        if (Input.GetKey(KeyCode.LeftShift) && _stamina > 0.0f)
+        if (Input.GetKey(KeyCode.LeftShift) && gameManagerScript._stamina > 0.0f)
         {
-            float staminaRemove = Time.deltaTime * 100 / _timeStaminaMaxUse;
+            float staminaRemove = Time.deltaTime * 100 / gameManagerScript._timeStaminaMaxUse;
             addStamina(-staminaRemove);
             _speed *= 2.0f;
-            _lastTimeStamina = Time.time;
+            gameManagerScript._lastTimeStamina = Time.time;
         }
 
         // Gain stamina when you don't run 
-        if (_lastTimeStamina + _timeStaminaGain < Time.time && _stamina < _staminaMax) addStamina(Time.deltaTime * 100 / 3.0f);
+        if (gameManagerScript._lastTimeStamina + gameManagerScript._timeStaminaGain < Time.time && gameManagerScript._stamina < gameManagerScript._staminaMax) addStamina(Time.deltaTime * 100 / 3.0f);
 
         // Lose food bar, in _timeFoodDeleteAll secondes, the food goes from 100 to 0.
-        if (_food > 0.0f) addFood(-Time.deltaTime * 100 / _timeFoodDeleteAll);
+        if (gameManagerScript._food > 0.0f) addFood(-Time.deltaTime * 100 / gameManagerScript._timeFoodDeleteAll);
         else SceneManager.LoadScene("MenuOver");
 
         float h = Input.GetAxisRaw("Horizontal");
@@ -96,10 +73,9 @@ public class scriptPlayer : MonoBehaviour
         transform.Translate(Vector3.forward * _speed * v * Time.deltaTime);
 
         // Saut
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
         {
-            teacherAnim.SetTrigger("jump");
-
+            _isGrounded = false;
             _rb.AddForce(Vector3.up * _jumpForce * Time.deltaTime, ForceMode.Impulse);
         }
 
@@ -120,46 +96,32 @@ public class scriptPlayer : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             if (bIsInDistributeur) addFood(15);
-            if (bIsInHospital) addHealth(_healthMax);
-            if (bIsInChildSave && childGameCollider != null)
-            {
-                //Destroy(childGameCollider.gameObject);
-                //Destroy(childGameCollider.transform.parent.gameObject);
-
-               //addSaveChild();
-            }
+            if (bIsInHospital) addHealth(gameManagerScript._healthMax);
         }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        UpdateUI();
-
         // Contrôles souris
         float horizontalOffset = _horizontalSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
         transform.Rotate(0.0f, horizontalOffset, 0.0f, Space.World);
-
-        if (_health <= 0)
-        {
-            SceneManager.LoadScene("MenuOver");
-        }
     }
 
     void OnCollisionStay(Collision collisionInfo)
     {
         for (int i = 0; i < collisionInfo.contactCount; i++)
         {
-            if (transform.position.x == collisionInfo.GetContact(i).point.x)
-            {
-                _isGrounded = true;
-            }
-
             if (collisionInfo.gameObject.tag == "Ennemy")
             {
                 addHealth(-1);
             }
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _isGrounded = true;
     }
 
     private void OnTriggerEnter(Collider collisionInfo)
@@ -171,29 +133,16 @@ public class scriptPlayer : MonoBehaviour
         }
         else if (collisionInfo.tag == "HitboxDistributerBuy") bIsInDistributeur = true;
         else if (collisionInfo.tag == "HitboxHospitalHeal") bIsInHospital = true;
-        else if (collisionInfo.tag == "HitboxChildSave") bIsInChildSave = true;
     }
 
     private void OnTriggerExit(Collider collisionInfo)
     {
         if (collisionInfo.tag == "HitboxDistributerBuy") bIsInDistributeur = false;
         else if (collisionInfo.tag == "HitboxHospitalHeal") bIsInHospital = false;
-        else if (collisionInfo.tag == "HitboxChildSave") bIsInChildSave = false;
     }
 
     private void OnTriggerStay(Collider collisionInfo)
     {
-        if (collisionInfo.tag == "HitboxChildSave")
-        {
-            childGameCollider = collisionInfo;
-
-            // Afficher : "Appuyer sur E Sauver"
-            //Print()
-        }
-        else
-        {
-            childGameCollider = null;
-        }
         if (collisionInfo.tag == "HitboxDistributerBuy")
         {
             // Afficher : "Appuyer sur E Acheter"
@@ -206,46 +155,21 @@ public class scriptPlayer : MonoBehaviour
         }
     }
 
-    private void UpdateHealth()
-    {
-        float pourcentHealth = _health * 100 / _healthMax;
-        _healthComponent.transform.localScale = new Vector3(pourcentHealth * 2 / 100, 0.2f, 1f);
-    }
-
-    private void UpdateStamina()
-    {
-        float pourcentStamina = _stamina * 100 / _staminaMax;
-        _staminaComponent.transform.localScale = new Vector3(pourcentStamina * 2 / 100, 0.2f, 1f);
-    }
-
-    private void UpdateFood()
-    {
-        float pourcentFood = _food * 100 / _foodMax;
-        _foodComponent.transform.localScale = new Vector3(pourcentFood * 2 / 100, 0.2f, 1f);
-    }
-
     public void addFood(float food)
     {
-        if (_food + food > _foodMax) _food = _foodMax;
-        else _food += food;
+        if (gameManagerScript._food + food > gameManagerScript._foodMax) gameManagerScript._food = gameManagerScript._foodMax;
+        else gameManagerScript._food += food;
     }
 
     public void addStamina(float stamina)
     {
-        if (_stamina + stamina > _staminaMax) _stamina = _staminaMax;
-        else _stamina += stamina;
+        if (gameManagerScript._stamina + stamina > gameManagerScript._staminaMax) gameManagerScript._stamina = gameManagerScript._staminaMax;
+        else gameManagerScript._stamina += stamina;
     }
 
     public void addHealth(float health)
     {
-        if (_health + health > _healthMax) _health = _healthMax;
-        else _health += health;
-    }
-
-    private void UpdateUI()
-    {
-        UpdateHealth();
-        UpdateStamina();
-        UpdateFood();
+        if (gameManagerScript._health + health > gameManagerScript._healthMax) gameManagerScript._health = gameManagerScript._healthMax;
+        else gameManagerScript._health += health;
     }
 }
